1cddadf0d5e1d828b0077f131ae559964c67022a
sanjana.gupta <sanjana.gupta@advarra.com>
2025-12-18 00:27:52 +0530

    add few missing files for cocoon js added in vendor

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "cocoon-js" # @0.0.5
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery" # @3.7.1
